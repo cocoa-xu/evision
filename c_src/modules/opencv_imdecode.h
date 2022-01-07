9a119403005f2fda2c1de5a242adc92029fa28c3
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2022-01-07 04:21:25 +0000

    imdecode

--- c_src/modules/opencv_imdecode.h
+++ c_src/modules/opencv_imdecode.h
@@ -19,11 +19,13 @@ static ERL_NIF_TERM evision_cv_imdecode(ErlNifEnv *env, int argc, const ERL_NIF_
     {
         ERL_NIF_TERM erl_binary = evision_get_kw(env, erl_terms, "buf");
         ErlNifBinary buf;
+        int flags;
         if (enif_inspect_binary(env, erl_binary, &buf) &&
             evision_to_safe(env, evision_get_kw(env, erl_terms, "flags"), flags, ArgInfo("flags", 0))) {
             Mat retval;
             int error_flag = false;
-            ERRWRAP2(retval = cv::imdecode(buf, flags), env, error_flag, error_term);
+            cv::Mat bufMat = cv::Mat(1, buf.size, CV_8UC1, buf.data);
+            ERRWRAP2(retval = cv::imdecode(bufMat, flags), env, error_flag, error_term);
             if (!error_flag) {
                 return evision::nif::ok(env, evision_from(env, retval));
             }
