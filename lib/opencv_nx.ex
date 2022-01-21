5080b0805e3d8f92153d0565fb98fb5684b03428
Cocoa <r.xu.1@research.gla.ac.uk>
2022-01-21 19:52:52 +0000

    add `OpenCV.Nx.to_mat`

--- lib/opencv_nx.ex
+++ lib/opencv_nx.ex
@@ -3,7 +3,7 @@ defmodule OpenCV.Nx do
   OpenCV mat to Nx tensor. 
 
   `:nx` is an optional dependency, so if you want to use
-  `OpenCV.Nx.to_nx/1`, you need to add it to the dependency
+  functions in `OpenCV.Nx`, you need to add it to the dependency
   list.
   """
 
@@ -19,7 +19,8 @@ defmodule OpenCV.Nx do
    >
   ```
   """
-  @doc namespace: :"external.Nx"
+  @doc namespace: :external
+  @spec to_mat(reference()) :: {:ok, reference()} | {:error, String.t()}
   def to_nx(mat) do
     if Code.ensure_loaded?(Nx) do
       {:ok, mat_type} = OpenCV.Mat.type(mat)
@@ -32,13 +33,50 @@ defmodule OpenCV.Nx do
           |> Nx.reshape(mat_shape)
 
         {:error, reason} ->
-          {:error, reason}
+          {:error, List.to_string(reason)}
 
         _ ->
           {:error, "unknown error"}
       end
     else
       {:error, ":nx is missing"}
+    end
+  end
+
+  @doc """
+  Converts a tensor of `Nx` to `Mat` of evision (OpenCV).
+  """
+  @doc namespace: :external
+  @spec to_mat(Nx.t()) :: {:ok, reference()} | {:error, String.t()}
+  def to_mat(nil, _), do: {:error, "tensor is nil"}
+
+  def to_mat(t) do
+    if Code.ensure_loaded?(Nx) do
+      {rows, cols, channels} = Nx.shape(t)
+      to_mat(Nx.to_binary(t), Nx.type(t), cols, rows, channels)
+    else
+      {:error, ":nx is missing"}
+    end
+  end
+
+  @doc false
+  @spec to_mat(
+          binary(),
+          {atom(), pos_integer()},
+          pos_integer(),
+          pos_integer(),
+          pos_integer()
+        ) :: {:ok, reference()} | {:error, charlist()}
+  def to_mat(binary, type, cols, rows, channels) do
+    case OpenCV.Mat.from_binary(binary, type, cols, rows, channels) do
+      {:ok, mat} ->
+        {:ok, mat}
+
+      {:error, reason} ->
+        {:error, List.to_string(reason)}
+
+      _ ->
+        {:error, "unknown error"}
     end
   end
 end
