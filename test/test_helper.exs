77e5a6fd1dcf46faa7abc5a94bbd78733bc30a10
Cocoa <r.xu.1@research.gla.ac.uk>
2022-01-22 21:52:08 +0000

    test: using `:require_downloading` tag for tests that require downloading test data

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -31,15 +31,18 @@ compiled_modules = OpenCV.__enabled_modules__()
 
 ExUnit.configure(
   exclude: [
+    # exclude all tests that requires downloading test data by default
+    require_downloading: true,
     # exclude dnn tests by default as it will download a large (~200 MB) model file
-    dnn: true,
+    dnn: !Enum.member?(compiled_modules, "dnn"),
+    ml: !Enum.member?(compiled_modules, "ml"),
+    # test OpenCV.Nx module if we have Nx module
+    nx: !Code.ensure_loaded?(Nx),
+    photo: !Enum.member?(compiled_modules, "photo"),
     # exclude video tests by default as video module can compile without decoding capability
     # and there is perhaps no way to test input from a camera
     # (could set up a virtual camera, but let's leave that for now)
-    video: true,
-    # test OpenCV.Nx module if we have Nx module
-    nx: !Code.ensure_loaded?(Nx),
-    ml: !Enum.member?(compiled_modules, "ml")
+    video: true
   ]
 )
 
