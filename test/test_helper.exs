4776425bd274312a270416cf1d269b606fbaad23
Cocoa <r.xu.1@research.gla.ac.uk>
2022-01-22 22:13:25 +0000

    test: add tag `:require_ffmpeg`

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -31,18 +31,21 @@ compiled_modules = OpenCV.__enabled_modules__()
 
 ExUnit.configure(
   exclude: [
-    # exclude all tests that requires downloading test data by default
+    # exclude all tests that require downloading test data by default
     require_downloading: true,
+    # exclude all tests that require ffmpeg by default
+    # at the moment, this excludes video tests by default
+    # as video module can compile without decoding capability
+    # and there is perhaps no way to test input from a camera
+    # (could set up a virtual camera, but let's leave that for now)
+    require_ffmpeg: true,
     # exclude dnn tests by default as it will download a large (~200 MB) model file
     dnn: !Enum.member?(compiled_modules, "dnn"),
     ml: !Enum.member?(compiled_modules, "ml"),
     # test OpenCV.Nx module if we have Nx module
     nx: !Code.ensure_loaded?(Nx),
     photo: !Enum.member?(compiled_modules, "photo"),
-    # exclude video tests by default as video module can compile without decoding capability
-    # and there is perhaps no way to test input from a camera
-    # (could set up a virtual camera, but let's leave that for now)
-    video: true
+    video: !Enum.member?(compiled_modules, "video")
   ]
 )
 
