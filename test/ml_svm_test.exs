175cc445ec783e48987d08d0cf7c86f058f9e37e
Cocoa <r.xu.1@research.gla.ac.uk>
2022-01-22 03:43:24 +0000

    test: add test for `OpenCV.ML.SVM`

---
+++ test/ml_svm_test.exs
@@ -0,0 +1,72 @@
+defmodule OpenCV.ML.SVM.Test do
+  use ExUnit.Case
+
+  @moduletag timeout: 120_000
+
+  @tag :ml
+  test "svm" do
+    labels = [1, -1, -1, -1]
+    training_data = [[501, 10], [255, 10], [501, 255], [10, 501]]
+
+    {:ok, labels_mat} =
+      OpenCV.Mat.from_binary(Enum.into(labels, <<>>, fn d -> << d::integer()-size(32)-little >> end), {:s, 32}, 1, 4, 1)
+
+    {:ok, training_data_mat} =
+      OpenCV.Mat.from_binary(
+        Enum.into(List.flatten(training_data), <<>>, fn d -> << d::float()-size(32)-little >> end),
+        {:f, 32},
+        2,
+        4,
+        1
+      )
+
+    {:ok, svm} = OpenCV.ML.SVM.create()
+    {:ok, _svm} = OpenCV.ML.SVM.setType(svm, OpenCV.cv_C_SVC())
+    {:ok, _svm} = OpenCV.ML.SVM.setKernel(svm, OpenCV.cv_LINEAR())
+    {:ok, _svm} = OpenCV.ML.SVM.setTermCriteria(svm, {OpenCV.cv_MAX_ITER(), 100, 0.000001})
+    assert :ok = OpenCV.ML.SVM.train(svm, training_data_mat, OpenCV.cv_ROW_SAMPLE(), labels_mat)
+    assert :ok = OpenCV.ML.SVM.isTrained(svm)
+
+    {:ok, sv} = OpenCV.ML.SVM.getUncompressedSupportVectors(svm)
+    {:ok, {rows, cols}} = OpenCV.Mat.shape(sv)
+    {:ok, sv_binary} = OpenCV.Mat.to_binary(sv)
+    float_bytes = 4
+    support_vector =
+      for i <- (rows-1)..0, reduce: [] do
+        support_vector ->
+          current_vector = for j <- (cols-1)..0, reduce: [] do
+            vec ->
+              << float_data::float()-size(32)-little >> = :binary.part(sv_binary, (i*cols + j) * float_bytes, 4)
+              [float_data | vec]
+          end
+          [current_vector | support_vector]
+      end
+    assert [[501.0, 10.0], [255.0, 10.0], [501.0, 255.0]] == support_vector
+
+    green = [0, 255, 0]
+    blue = [255, 0, 0]
+    width = 512
+    height = 512
+    response_data =
+      for x <- (width-1)..0, y <- (height-1)..0, reduce: [] do
+        acc ->
+          {:ok, sample} = OpenCV.Mat.from_binary(<< y::float()-size(32)-little, x::float()-size(32)-little >>, {:f, 32}, 2, 1, 1)
+          {:ok, {_, response_mat}} = OpenCV.ML.SVM.predict(svm, sample)
+          assert {:ok, {1, 1}} = OpenCV.Mat.shape(response_mat)
+          {:ok, << response::float()-size(32)-little >>} = OpenCV.Mat.to_binary(response_mat)
+          response = trunc(response)
+          assert response == 1 or response == -1
+          case response do
+            1 ->
+              [green | acc]
+            -1 ->
+              [blue | acc]
+          end
+      end
+    response_data = response_data |> List.flatten |> IO.iodata_to_binary
+
+    {:ok, mat} = OpenCV.imread(Path.join(__DIR__, ["svm_test.png"]))
+    {:ok, expected} = OpenCV.Mat.to_binary(mat)
+    assert expected == response_data
+  end
+end
