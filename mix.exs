bd79a7149a7cb1de89fcc684f4c1e8374595b081
Cocoa <r.xu.1@research.gla.ac.uk>
2022-09-29 01:52:33 +0100

    start v0.1.7-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Evision.MixProject do
   require Logger
 
   @app :evision
-  @version "0.1.6"
+  @version "0.1.7-dev"
   @last_released_version "0.1.6"
   @github_url "https://github.com/cocoa-xu/evision"
   @opencv_version "4.6.0"
