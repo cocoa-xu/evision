b33537bfc91f1e1428c437387330bb243d5eb013
Cocoa <r.xu.1@research.gla.ac.uk>
2022-10-08 05:49:19 +0100

    fixed TLS warnings

--- mix.exs
+++ mix.exs
@@ -157,6 +157,160 @@ defmodule Mix.Tasks.Compile.EvisionPrecompiled do
     available_for_target?
   end
 
+  # https_opts and related code are taken from
+  # https://github.com/elixir-cldr/cldr_utils/blob/master/lib/cldr/http/http.ex
+  @certificate_locations [
+    # Configured cacertfile
+    System.get_env("ELIXIR_MAKE_CACERT"),
+
+    # Populated if hex package CAStore is configured
+    if(Code.ensure_loaded?(CAStore), do: CAStore.file_path()),
+
+    # Populated if hex package certfi is configured
+    if(Code.ensure_loaded?(:certifi),
+      do: :certifi.cacertfile() |> List.to_string()
+    ),
+
+    # Debian/Ubuntu/Gentoo etc.
+    "/etc/ssl/certs/ca-certificates.crt",
+
+    # Fedora/RHEL 6
+    "/etc/pki/tls/certs/ca-bundle.crt",
+
+    # OpenSUSE
+    "/etc/ssl/ca-bundle.pem",
+
+    # OpenELEC
+    "/etc/pki/tls/cacert.pem",
+
+    # CentOS/RHEL 7
+    "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem",
+
+    # Open SSL on MacOS
+    "/usr/local/etc/openssl/cert.pem",
+
+    # MacOS & Alpine Linux
+    "/etc/ssl/cert.pem"
+  ]
+  |> Enum.reject(&is_nil/1)
+
+  @doc false
+  def certificate_store do
+    @certificate_locations
+    |> Enum.find(&File.exists?/1)
+    |> raise_if_no_cacertfile!
+    |> :erlang.binary_to_list()
+  end
+
+  defp raise_if_no_cacertfile!(nil) do
+    raise RuntimeError, """
+    No certificate trust store was found.
+    Tried looking for: #{inspect(@certificate_locations)}
+    A certificate trust store is required in
+    order to download locales for your configuration.
+    Since elixir_make could not detect a system
+    installed certificate trust store one of the
+    following actions may be taken:
+    1. Install the hex package `castore`. It will
+      be automatically detected after recompilation.
+    2. Install the hex package `certifi`. It will
+      be automatically detected after recomilation.
+    3. Specify the location of a certificate trust store
+      by configuring it in `config.exs`:
+        config :elixir_make,
+        cacertfile: "/path/to/cacertfile",
+        ...
+    """
+  end
+
+  defp raise_if_no_cacertfile!(file) do
+    file
+  end
+
+  defp https_opts(hostname) do
+    if secure_ssl?() do
+      [
+        ssl: [
+          verify: :verify_peer,
+          cacertfile: certificate_store(),
+          depth: 4,
+          ciphers: preferred_ciphers(),
+          versions: protocol_versions(),
+          eccs: preferred_eccs(),
+          reuse_sessions: true,
+          server_name_indication: hostname,
+          secure_renegotiate: true,
+          customize_hostname_check: [
+            match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+          ]
+        ]
+      ]
+    else
+      [
+        ssl: [
+          verify: :verify_none,
+          server_name_indication: hostname,
+          secure_renegotiate: true,
+          reuse_sessions: true,
+          versions: protocol_versions(),
+          ciphers: preferred_ciphers(),
+          versions: protocol_versions()
+        ]
+      ]
+    end
+  end
+
+  def preferred_ciphers do
+    preferred_ciphers = [
+      # Cipher suites (TLS 1.3): TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256
+      %{cipher: :aes_128_gcm, key_exchange: :any, mac: :aead, prf: :sha256},
+      %{cipher: :aes_256_gcm, key_exchange: :any, mac: :aead, prf: :sha384},
+      %{cipher: :chacha20_poly1305, key_exchange: :any, mac: :aead, prf: :sha256},
+      # Cipher suites (TLS 1.2): ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:
+      # ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:
+      # ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384
+      %{cipher: :aes_128_gcm, key_exchange: :ecdhe_ecdsa, mac: :aead, prf: :sha256},
+      %{cipher: :aes_128_gcm, key_exchange: :ecdhe_rsa, mac: :aead, prf: :sha256},
+      %{cipher: :aes_256_gcm, key_exchange: :ecdh_ecdsa, mac: :aead, prf: :sha384},
+      %{cipher: :aes_256_gcm, key_exchange: :ecdh_rsa, mac: :aead, prf: :sha384},
+      %{cipher: :chacha20_poly1305, key_exchange: :ecdhe_ecdsa, mac: :aead, prf: :sha256},
+      %{cipher: :chacha20_poly1305, key_exchange: :ecdhe_rsa, mac: :aead, prf: :sha256},
+      %{cipher: :aes_128_gcm, key_exchange: :dhe_rsa, mac: :aead, prf: :sha256},
+      %{cipher: :aes_256_gcm, key_exchange: :dhe_rsa, mac: :aead, prf: :sha384}
+    ]
+
+    :ssl.filter_cipher_suites(preferred_ciphers, [])
+  end
+
+  def protocol_versions do
+    if otp_version() < 25 do
+      [:"tlsv1.2"]
+    else
+      [:"tlsv1.2", :"tlsv1.3"]
+    end
+  end
+
+  def otp_version do
+    :erlang.system_info(:otp_release) |> List.to_integer()
+  end
+
+  def preferred_eccs do
+    # TLS curves: X25519, prime256v1, secp384r1
+    preferred_eccs = [:secp256r1, :secp384r1]
+    :ssl.eccs() -- :ssl.eccs() -- preferred_eccs
+  end
+
+  def secure_ssl? do
+    case System.get_env("ELIXIR_MAKE_UNSAFE_HTTPS") do
+      nil -> true
+      "FALSE" -> false
+      "false" -> false
+      "nil" -> false
+      "NIL" -> false
+      _other -> true
+    end
+  end
+
   def download!(url, save_as, overwrite \\ false)
 
   def download!(url, save_as, false) do
@@ -168,11 +322,14 @@ defmodule Mix.Tasks.Compile.EvisionPrecompiled do
   end
 
   def download!(url, save_as, true) do
-    http_opts = []
     opts = [body_format: :binary]
     arg = {url, []}
 
-    case :httpc.request(:get, arg, http_opts, opts) do
+    # https://erlef.github.io/security-wg/secure_coding_and_deployment_hardening/inets
+    # TODO: This may no longer be necessary from Erlang/OTP 26.0 or later.
+    http_options = https_opts(String.to_charlist(URI.parse(url).host))
+
+    case :httpc.request(:get, arg, http_options, opts) do
       {:ok, {{_, 200, _}, _, body}} ->
         File.write!(save_as, body)
         checksum(save_as)
@@ -344,7 +501,7 @@ defmodule Mix.Tasks.Compile.EvisionPrecompiled do
     end
 
     Logger.info("Copying priv directory: #{cached_priv_dir} => #{app_priv}")
-    with {"", 0} <- File.cp_r(cached_priv_dir, app_priv) do
+    with {:ok, _} <- File.cp_r(cached_priv_dir, app_priv) do
       :ok
     else
       {msg, code} ->
@@ -354,7 +511,7 @@ defmodule Mix.Tasks.Compile.EvisionPrecompiled do
     end
 
     Logger.info("Copying generated Elixir binding files: #{cached_elixir_dir} => #{generated_elixir_dir}")
-    with {"", 0} <- File.cp_r(cached_elixir_dir, generated_elixir_dir) do
+    with {:ok, _} <- File.cp_r(cached_elixir_dir, generated_elixir_dir) do
       :ok
     else
       {msg, code} ->
