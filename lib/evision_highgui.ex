d3fbe8109cfd306266ff9162a216e578ed69b98b
Cocoa <r.xu.1@research.gla.ac.uk>
2022-10-09 12:33:06 +0100

    added @spec for functions in `Evision.HighGui`

--- lib/evision_highgui.ex
+++ lib/evision_highgui.ex
@@ -8,29 +8,48 @@ defmodule Evision.HighGui do
   @doc """
   Show a mat in a named window
 
-  - **winname**. The name of the window.
-  - **mat**. The image.
+  #### Positional Arguments
 
-  ## Example
+  - **winname**. `String`
+
+    The name of the window.
+
+  - **mat**. `Evision.Mat`
+
+    The image.
+
+  #### Example
+
   ```elixir
-  {:ok, mat} = Evision.imread("example.jpg")
-  Evision.imshow("OpenCV", mat)
+  iex> mat = Evision.imread!("example.jpg")
+  iex> Evision.imshow("OpenCV", mat)
   # the following line may be necessary on your system
   # will try to improve this later
-  Evision.waitkey(0)
+  iex> Evision.waitkey(0)
   ```
+
   """
   @doc namespace: :"cv.highgui"
-  def imshow(winname, mat) when is_binary(winname) do
+  @spec imshow(String.t(), reference() | Evision.Mat.t() | Nx.Tensor.t()) :: :ok | {:error, String.t()}
+  def imshow(winname, mat) when is_binary(winname) and (is_reference(mat) or is_struct(mat))do
+    mat = Evision.Internal.Structurise.from_struct(mat)
     :evision_nif.imshow(winname: winname, mat: mat)
   end
 
   deferror(imshow(winname, mat))
 
   @doc """
-  wait for user keyboard event for a `delay` amount of time (ms)
+  Wait for user keyboard event for a `delay` amount of time (ms)
+
+  #### Positional Arguments
+
+  - **delay**. `int`
+
+    Wait for `delay` ms.
+
   """
   @doc namespace: :"cv.highgui"
+  @spec waitkey(integer()) :: :ok | {:error, String.t()}
   def waitkey(delay) when is_integer(delay) do
     :evision_nif.waitKey(delay: delay)
   end
@@ -38,9 +57,17 @@ defmodule Evision.HighGui do
   deferror(waitkey(delay))
 
   @doc """
-  close a named window
+  Close a named window
+
+  #### Positional Arguments
+
+  - **winname**. `String`
+
+    The name of the window.
+
   """
   @doc namespace: :"cv.highgui"
+  @spec destroyWindow(String.t()) :: :ok | {:error, String.t()}
   def destroyWindow(winname) when is_binary(winname) do
     :evision_nif.destroyWindow(winname: winname)
   end
@@ -48,9 +75,10 @@ defmodule Evision.HighGui do
   deferror(destroyWindow(winname))
 
   @doc """
-  close all windows
+  Close all windows
   """
   @doc namespace: :"cv.highgui"
+  @spec destroyAllWindows() :: :ok
   def destroyAllWindows do
     :evision_nif.destroyAllWindows()
   end
