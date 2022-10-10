0b847481fe4044a2cc35f658897915a34cc036fa
Cocoa <r.xu.1@research.gla.ac.uk>
2022-10-10 11:02:16 +0100

    patch: using a python script to apply patches so that it works on all platforms

---
+++ patches/apply_patch.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from pathlib import Path
+if sys.version_info[0] >= 3:
+    from io import StringIO
+else:
+    from cStringIO import StringIO
+
+
+def patch_fix_getLayerShapes(opencv_version: str, opencv_src_root: str):
+    if opencv_version not in ['4.5.4', '4.5.5', '4.6.0']:
+        print(f"warning: applying `patch_fix_getLayerShapes` to opencv version `{opencv_version}`")
+
+    # modules/dnn/include/opencv2/dnn/dnn.hpp
+    dnn_hpp = Path(opencv_src_root) / 'modules' / 'dnn' / 'include' / 'opencv2' / 'dnn' / 'dnn.hpp'
+    fixed = StringIO()
+    patched_1 = False
+    patched_2 = False
+    with open(dnn_hpp, 'r') as source:
+        for line in source:
+            if not patched_1 and line.strip() == 'void getLayerShapes(const MatShape& netInputShape,':
+                fixed.write('        CV_WRAP void getLayerShapes(const MatShape& netInputShape,\n')
+                patched_1 = True
+            elif not patched_2 and line.strip() == 'void getLayerShapes(const std::vector<MatShape>& netInputShapes,':
+                fixed.write('        CV_WRAP void getLayerShapes(const std::vector<MatShape>& netInputShapes,\n')
+                patched_2 = True
+            else:
+                fixed.write(line)
+
+    with open(dnn_hpp, 'w') as dst:
+        dst.truncate(0)
+        dst.write(fixed.getvalue())
+
+
+patches = [patch_fix_getLayerShapes]
+
+
+if __name__ == '__main__':
+    cv_version = None
+    cv_src_root = None
+    if len(sys.argv) != 3:
+        sys.exit(1)
+    cv_src_root = sys.argv[1]
+    cv_version = sys.argv[2]
+    print(cv_version, cv_src_root)
+    for p in patches:
+        p(cv_version, cv_src_root)
