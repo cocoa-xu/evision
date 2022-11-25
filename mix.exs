06c773de48c603f8d8c78ea39d6fee5ee9237ca2
Cocoa <r.xu.1@research.gla.ac.uk>
2022-11-25 14:33:22 +0000

    remove unused lines

--- mix.exs
+++ mix.exs
@@ -16,29 +16,6 @@ defmodule Mix.Tasks.Compile.EvisionPrecompiled do
   require Logger
   alias Evision.MixProject.Metadata
 
-  @available_versions [
-    "0.1.21",
-    "0.1.19",
-    "0.1.18",
-    "0.1.17",
-    "0.1.16",
-    "0.1.15",
-    "0.1.14",
-    "0.1.13",
-    "0.1.12",
-    "0.1.11",
-    "0.1.9",
-    "0.1.8",
-    "0.1.7",
-    "0.1.6",
-    "0.1.5",
-    "0.1.4",
-    "0.1.3",
-    "0.1.2",
-    "0.1.1",
-    "0.1.0-dev"
-  ]
-
   @available_targets [
     "aarch64-apple-darwin",
     "x86_64-apple-darwin",
