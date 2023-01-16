d0419311c977be8dc2b206122c170634a28f5b99
Cocoa <i@uwucocoa.moe>
2023-01-16 00:59:33 +0000

    [test] test if `Evision.CUDA.GpuMat` is loaded

--- test/evision_gpumat_test.exs
+++ test/evision_gpumat_test.exs
@@ -1,109 +1,114 @@
 defmodule Evision.CUDA.GpuMat.Test do
   use ExUnit.Case
 
+  @compile {:no_warn_undefined, Evision.CUDA.GpuMat}
+
   alias Evision.Mat
-  alias Evision.CUDA.GpuMat
 
   @tag :require_cuda
   describe "Basic Operations" do
-    test "load an image from file" do
-      %Mat{} = mat = Evision.imread(Path.join([__DIR__, "testdata", "test.png"]))
+    if Code.ensure_loaded?(Evision.CUDA.GpuMat) do
+      alias Evision.CUDA.GpuMat
 
-      gpumat = GpuMat.gpuMat(mat)
+      test "load an image from file" do
+        %Mat{} = mat = Evision.imread(Path.join([__DIR__, "testdata", "test.png"]))
 
-      %GpuMat{
-        channels: 3,
-        type: {:u, 8},
-        raw_type: 16,
-        shape: {2, 3, 3},
-        elemSize: 3
-      } = gpumat
-    end
+        gpumat = Evision.CUDA.GpuMat.gpuMat(mat)
 
-    test "explicitly upload an Evision.Mat" do
-      %Mat{} = mat = Evision.imread(Path.join([__DIR__, "testdata", "test.png"]))
+        %GpuMat{
+          channels: 3,
+          type: {:u, 8},
+          raw_type: 16,
+          shape: {2, 3, 3},
+          elemSize: 3
+        } = gpumat
+      end
 
-      gpumat = GpuMat.gpuMat()
+      test "explicitly upload an Evision.Mat" do
+        %Mat{} = mat = Evision.imread(Path.join([__DIR__, "testdata", "test.png"]))
 
-      gpumat = Evision.CUDA.GpuMat.upload(gpumat, mat)
-      assert Evision.CUDA.GpuMat.cudaPtr(gpumat) > 0
+        gpumat = GpuMat.gpuMat()
 
-      %GpuMat{
-        channels: 3,
-        type: {:u, 8},
-        raw_type: 16,
-        shape: {2, 3, 3},
-        elemSize: 3
-      } = gpumat
-    end
+        gpumat = Evision.CUDA.GpuMat.upload(gpumat, mat)
+        assert Evision.CUDA.GpuMat.cudaPtr(gpumat) > 0
 
-    test "manually allocate a GpuMat" do
-      gpumat = Evision.CUDA.GpuMat.gpuMat(1000, 1200, Evision.Constant.cv_8UC3())
-      assert Evision.CUDA.GpuMat.cudaPtr(gpumat) > 0
+        %GpuMat{
+          channels: 3,
+          type: {:u, 8},
+          raw_type: 16,
+          shape: {2, 3, 3},
+          elemSize: 3
+        } = gpumat
+      end
 
-      %GpuMat{
-        channels: 3,
-        type: {:u, 8},
-        raw_type: 16,
-        shape: {1000, 1200, 3},
-        elemSize: 3
-      } = gpumat
-    end
+      test "manually allocate a GpuMat" do
+        gpumat = Evision.CUDA.GpuMat.gpuMat(1000, 1200, Evision.Constant.cv_8UC3())
+        assert Evision.CUDA.GpuMat.cudaPtr(gpumat) > 0
 
-    test "split channels" do
-      %Mat{} = mat = Evision.imread(Path.join([__DIR__, "testdata", "test.png"]))
+        %GpuMat{
+          channels: 3,
+          type: {:u, 8},
+          raw_type: 16,
+          shape: {1000, 1200, 3},
+          elemSize: 3
+        } = gpumat
+      end
 
-      gpumat = GpuMat.gpuMat(mat)
-      [b, g, r] = Evision.CUDA.split(gpumat)
+      test "split channels" do
+        %Mat{} = mat = Evision.imread(Path.join([__DIR__, "testdata", "test.png"]))
 
-      %GpuMat{
-        channels: 1,
-        type: {:u, 8},
-        raw_type: 0,
-        shape: {2, 3, 1},
-        elemSize: 1
-      } = b
+        gpumat = GpuMat.gpuMat(mat)
+        [b, g, r] = Evision.CUDA.split(gpumat)
 
-      %GpuMat{
-        channels: 1,
-        type: {:u, 8},
-        raw_type: 0,
-        shape: {2, 3, 1},
-        elemSize: 1
-      } = g
+        %GpuMat{
+          channels: 1,
+          type: {:u, 8},
+          raw_type: 0,
+          shape: {2, 3, 1},
+          elemSize: 1
+        } = b
 
-      %GpuMat{
-        channels: 1,
-        type: {:u, 8},
-        raw_type: 0,
-        shape: {2, 3, 1},
-        elemSize: 1
-      } = r
-    end
+        %GpuMat{
+          channels: 1,
+          type: {:u, 8},
+          raw_type: 0,
+          shape: {2, 3, 1},
+          elemSize: 1
+        } = g
 
-    test "transpose" do
-      %Mat{} = mat = Evision.imread(Path.join([__DIR__, "testdata", "test.png"]))
+        %GpuMat{
+          channels: 1,
+          type: {:u, 8},
+          raw_type: 0,
+          shape: {2, 3, 1},
+          elemSize: 1
+        } = r
+      end
 
-      gpumat = GpuMat.gpuMat(mat)
-      [b, _, _] = Evision.CUDA.split(gpumat)
+      test "transpose" do
+        %Mat{} = mat = Evision.imread(Path.join([__DIR__, "testdata", "test.png"]))
 
-      %GpuMat{
-        channels: 1,
-        type: {:u, 8},
-        raw_type: 0,
-        shape: {2, 3, 1},
-        elemSize: 1
-      } = b
+        gpumat = GpuMat.gpuMat(mat)
+        [b, _, _] = Evision.CUDA.split(gpumat)
 
-      bT = Evision.CUDA.transpose(b)
+        %GpuMat{
+          channels: 1,
+          type: {:u, 8},
+          raw_type: 0,
+          shape: {2, 3, 1},
+          elemSize: 1
+        } = b
 
-      %GpuMat{
-        channels: 1,
-        type: {:u, 8},
-        raw_type: 0,
-        shape: {3, 2, 1},
-        elemSize: 1
-      } = bT
+        bT = Evision.CUDA.transpose(b)
+
+        %GpuMat{
+          channels: 1,
+          type: {:u, 8},
+          raw_type: 0,
+          shape: {3, 2, 1},
+          elemSize: 1
+        } = bT
+      end
     end
   end
 end
