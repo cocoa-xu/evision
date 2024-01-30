265791089b2f256007fd115f05328e8a1580657d
Cocoa <i@uwucocoa.moe>
2024-01-30 14:55:18 +0700

    [ci-linux-x86_64] try to use precompiled erlang

--- .github/do-build-manylinux2014.sh
+++ .github/do-build-manylinux2014.sh
@@ -4,32 +4,18 @@ set -x
 
 MIX_ENV=$1
 OTP_VERSION=$2
-ELIXIR_VERSION=$3
-EVISION_ENABLE_CONTRIB=$4
+HOST_ARCHITECTURE=$3
+ELIXIR_VERSION=$4
+EVISION_ENABLE_CONTRIB=$5
+          
+export PATH=/work/cache/otp/usr/local/bin:/work/cache/elixir/elixir-${ELIXIR_VERSION}/bin:${PATH}
+export ERL_ROOTDIR=/work/cache/otp/usr/local/lib/erlang
+export CMAKE_OPENCV_OPTIONS="-D CMAKE_C_FLAGS=\"-static-libgcc -static-libstdc++\" -D CMAKE_CXX_FLAGS=\"-static-libgcc -static-libstdc++\""
+export CMAKE_EVISION_OPTIONS="-D CMAKE_C_FLAGS=\"-static-libgcc -static-libstdc++\" -D CMAKE_CXX_FLAGS=\"-static-libgcc -static-libstdc++\""
 
-OPENSSL_VERSION="3.1.1"
-ASDF_VERSION="v0.12.0"
-
-yum install -y openssl-devel ncurses-devel perl-IPC-Cmd python3 && \
-    cd / && \
-    curl -fSL https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz -o openssl-${OPENSSL_VERSION}.tar.gz && \
-    tar xf openssl-${OPENSSL_VERSION}.tar.gz && \
-    cd openssl-${OPENSSL_VERSION} && \
-    ./Configure && make && \
-    make DESTDIR="/openssl-${OPENSSL_VERSION}/openssl-build" install &&
-    cd / && \
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch ${ASDF_VERSION} && \
-    . "/root/.asdf/asdf.sh" && \
-    export KERL_CONFIGURE_OPTIONS="--without-javac --with-ssl=/openssl-${OPENSSL_VERSION}/openssl-build/usr/local" && \
-    asdf plugin add erlang && \
-    asdf plugin add elixir && \
-    asdf install erlang ${OTP_VERSION} && \
-    asdf install elixir ${ELIXIR_VERSION} && \
-    asdf global erlang ${OTP_VERSION} && \
-    asdf global elixir ${ELIXIR_VERSION} && \
-    export CMAKE_OPENCV_OPTIONS="-D CMAKE_C_FLAGS=\"-static-libgcc -static-libstdc++\" -D CMAKE_CXX_FLAGS=\"-static-libgcc -static-libstdc++\"" && \
-    export CMAKE_EVISION_OPTIONS="-D CMAKE_C_FLAGS=\"-static-libgcc -static-libstdc++\" -D CMAKE_CXX_FLAGS=\"-static-libgcc -static-libstdc++\"" && \
-    cd /work && \
+cd /work && \
+    mix local.hex --force && \
+    mix local.rebar --force && \
     mix deps.get && \
     mix compile_opencv
 
--- .github/script-to-build-manylinux2014.sh
+++ .github/script-to-build-manylinux2014.sh
@@ -4,12 +4,9 @@ set -x
 
 MIX_ENV=$1
 OTP_VERSION=$2
-ELIXIR_VERSION=$3
-EVISION_ENABLE_CONTRIB=$4
+HOST_ARCHITECTURE=$3
+ELIXIR_VERSION=$4
+EVISION_ENABLE_CONTRIB=$5
 
-sudo docker run --privileged --network=host --rm -v `pwd`:/work quay.io/pypa/manylinux2014_x86_64:latest \
-    sh -c "chmod a+x /work/do-build-manylinux2014.sh && /work/do-build-manylinux2014.sh ${MIX_ENV} ${OTP_VERSION} ${ELIXIR_VERSION} ${EVISION_ENABLE_CONTRIB}"
-
-if [ -d "`pwd`/_build" ]; then
-  sudo chmod -R a+wr `pwd`/_build ;
-fi
+sudo docker run --privileged --network=host --rm -v "$(pwd):/work" quay.io/pypa/manylinux2014_x86_64:latest \
+  sh -c "chmod a+x /work/do-build-manylinux2014.sh && /work/do-build-manylinux2014.sh ${MIX_ENV} ${OTP_VERSION} ${HOST_ARCHITECTURE} ${ELIXIR_VERSION} ${EVISION_ENABLE_CONTRIB}"
