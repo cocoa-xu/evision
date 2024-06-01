0bf92d14ce699977f04e37ca39a6849a8d67a03f
Cocoa <i@uwucocoa.moe>
2024-06-01 11:19:24 +0100

    updated do_release.sh

--- do_release.sh
+++ do_release.sh
@@ -9,15 +9,21 @@ export EVISION_PREFER_PRECOMPILED=true
 export EVISION_ENABLE_CUDA=true
 export EVISION_CUDA_VERSION=121
 export NIF_VERSION=2.16
-export EVISION_VERSION="$(grep 'def version, do: "' mix.exs | grep -E -oh '(\d+).(\d+).(\d+)')"
+export EVISION_VERSION="$(grep 'def version, do: "' mix.exs | grep -P -oh '(\d+).(\d+).(\d+)')"
 
-export TARBALL_FILENAME="evision-nif_${NIF_VERSION}-x86_64-linux-gnu-contrib-cuda${EVISION_CUDA_VERSION}-${EVISION_VERSION}.tar.gz"
-curl -fSL "https://github.com/cocoa-xu/evision/releases/download/v${EVISION_VERSION}/${TARBALL_FILENAME}" -o "/tmp/${TARBALL_FILENAME}"
-export TARBALL_SHA256="$(sha256sum "/tmp/${TARBALL_FILENAME}" | cut -d ' ' -f 1)"
+export TARBALL_FILENAME="evision-nif_${NIF_VERSION}-x86_64-linux-gnu-contrib-cuda${EVISION_CUDA_VERSION}-${EVISION_VERSION}"
+export TARBALL_NAME="${TARBALL_FILENAME}.tar.gz"
+curl -fSL "https://github.com/cocoa-xu/evision/releases/download/v${EVISION_VERSION}/${TARBALL_NAME}" -o "/tmp/${TARBALL_NAME}"
+export TARBALL_SHA256="$(sha256sum "/tmp/${TARBALL_NAME}" | cut -d ' ' -f 1)"
 cat <<EOF > checksum.exs
 %{
-  "${TARBALL_FILENAME}" => "sha256:${TARBALL_SHA256}"
+  "${TARBALL_NAME}" => "sha256:${TARBALL_SHA256}"
 }
 EOF
+rm -rf release
+mkdir -p release
+tar xf "/tmp/${TARBALL_NAME}" -C release
+rm -rf lib/generated
+mv "release/${TARBALL_FILENAME}/elixir_generated" lib/generated
 EVISION_FETCH_PRECOMPILED=true mix evision.fetch --all
 MIX_ENV=docs mix hex.publish
