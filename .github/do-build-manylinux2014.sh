882a7fb30429eecb58a895f30d458349f0b8a393
Cocoa <i@uwucocoa.moe>
2024-06-10 12:42:31 +0100

    added experimental ci build for gleam

--- .github/do-build-manylinux2014.sh
+++ .github/do-build-manylinux2014.sh
@@ -7,7 +7,8 @@ OTP_VERSION=$2
 HOST_ARCHITECTURE=$3
 ELIXIR_VERSION=$4
 EVISION_ENABLE_CONTRIB=$5
-          
+GLEAM_VERSION=$6
+
 export PATH=/work/cache/otp/usr/local/bin:/work/cache/elixir/elixir-${ELIXIR_VERSION}/bin:${PATH}
 export ERL_ROOTDIR=/work/cache/otp/usr/local/lib/erlang
 export CMAKE_OPENCV_OPTIONS="-D CMAKE_C_FLAGS=\"-static-libgcc -static-libstdc++\" -D CMAKE_CXX_FLAGS=\"-static-libgcc -static-libstdc++\""
@@ -25,6 +26,7 @@ cd /work
 export EVISION_ENABLE_CONTRIB="${EVISION_ENABLE_CONTRIB}"
 export MIX_ENV="${MIX_ENV}"
 export EVISION_PREFER_PRECOMPILED="false"
+export GLEAM_VERSION="${GLEAM_VERSION}"
 rm -f _build/${MIX_ENV}/lib/evision/priv/evision.so
 
 rm -rf lib/generated && rm -rf src/generated
