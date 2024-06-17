e62bfdbd84c339619d39c7fffe35e02b0601f111
Cocoa <i@uwucocoa.moe>
2024-06-17 14:47:00 +0100

    use `'` as delimiter for uppercase sigil `~S`

--- mix.exs
+++ mix.exs
@@ -1246,7 +1246,7 @@ defmodule Evision.MixProject do
   end
 
   defp before_closing_body_tag(:html) do
-    ~S(
+    ~S'
     <script type="text/x-mathjax-config">
     MathJax.Hub.Config({
         tex2jax: {
@@ -1257,7 +1257,7 @@ defmodule Evision.MixProject do
         },
         extensions: ["tex2jax.js", "TeX/AMSmath.js", "TeX/AMSsymbols.js"],
         jax: ["input/TeX", "output/HTML-CSS"],
-    }\);
+    });
     //<![CDATA[
     MathJax.Hub.Config(
     {
@@ -1270,19 +1270,19 @@ defmodule Evision.MixProject do
                 vecthree: ["\\begin{bmatrix} #1\\\\ #2\\\\ #3 \\end{bmatrix}", 3],
                 vecthreethree: ["\\begin{bmatrix} #1 & #2 & #3\\\\ #4 & #5 & #6\\\\ #7 & #8 & #9 \\end{bmatrix}", 9],
                 cameramatrix: ["#1 = \\begin{bmatrix} f_x & 0 & c_x\\\\ 0 & f_y & c_y\\\\ 0 & 0 & 1 \\end{bmatrix}", 1],
-                distcoeffs: ["(k_1, k_2, p_1, p_2[, k_3[, k_4, k_5, k_6 [, s_1, s_2, s_3, s_4[, \\tau_x, \\tau_y]]]]\) \\text{ of 4, 5, 8, 12 or 14 elements}"],
-                distcoeffsfisheye: ["(k_1, k_2, k_3, k_4\)"],
+                distcoeffs: ["(k_1, k_2, p_1, p_2[, k_3[, k_4, k_5, k_6 [, s_1, s_2, s_3, s_4[, \\tau_x, \\tau_y]]]]) \\text{ of 4, 5, 8, 12 or 14 elements}"],
+                distcoeffsfisheye: ["(k_1, k_2, k_3, k_4)"],
                 hdotsfor: ["\\dots", 1],
                 mathbbm: ["\\mathbb{#1}", 1],
                 bordermatrix: ["\\matrix{#1}", 1]
             }
         }
     }
-    \);
+    );
     //]]>
     </script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js"></script>
-    )
+    '
   end
 
   defp before_closing_body_tag(_), do: ""
