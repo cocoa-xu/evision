ebc76e36cbd2144af08e0f207ea6c738fa6276b8
Cocoa <i@uwucocoa.moe>
2025-01-18 20:25:44 +0100

    mix format
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- test/evision_face_test.exs
+++ test/evision_face_test.exs
@@ -2,9 +2,13 @@ defmodule Evision.Face.FaceRecognizerTest do
   use ExUnit.Case
 
   setup do
-    %Evision.Mat{} = image = Evision.imread(
-      Path.join([__DIR__, "testdata", "back.jpg"]), 
-      flags: Evision.Constant.cv_IMREAD_GRAYSCALE())
+    %Evision.Mat{} =
+      image =
+      Evision.imread(
+        Path.join([__DIR__, "testdata", "back.jpg"]),
+        flags: Evision.Constant.cv_IMREAD_GRAYSCALE()
+      )
+
     %{image: Evision.Mat.as_type(image, :s32)}
   end
 
@@ -19,7 +23,10 @@ defmodule Evision.Face.FaceRecognizerTest do
     test "Evision.Face.FisherFaceRecognizer.t()", %{image: image} do
       module = Evision.Face.FisherFaceRecognizer
       face_recogniser = module.create()
-      face_recogniser = module.train(face_recogniser, [image, image], Nx.tensor([0, 1], type: :s32))
+
+      face_recogniser =
+        module.train(face_recogniser, [image, image], Nx.tensor([0, 1], type: :s32))
+
       assert face_recogniser.__struct__ == module
     end
 
